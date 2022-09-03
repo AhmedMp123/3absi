787d024c6951f559f8e9ae08dbf0350784b918c0
AhmedMp123 <ahmedmp2019c@gmail.com>
2022-09-03 19:42:55 +0200

    this is new program

---
+++ Untitled1.c
@@ -0,0 +1,13 @@
+#include <Stdio.h>
+int main (void){
+    int x;
+   int count=0;
+
+printf("please eneter a number\n");
+scanf("%d",&x);
+while (x%10!=0){
+    x/=10;
+    count++;
+}
+printf("the answer is :- %d",count);
+}
